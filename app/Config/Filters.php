e6e81799bdae0e8971ed4a4097a669fb5dde147c
Ikhsansir2003 <ikhsansiregar42003@gmail.com>
2024-04-03 12:18:03 +0700

    Middleware Yang Melindungi Seluruh Umat Manusia -> Aku Cinta Icha

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -33,7 +33,7 @@ class Filters extends BaseConfig
      *
      * @var array<string, array<string, array<string, string>>>|array<string, list<string>>
      */
-    public array $globals = [
+    public array $globals = [ 
         'before' => [
             // 'honeypot',
             // 'csrf',
